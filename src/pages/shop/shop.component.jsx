286c983ab26cefdc011430766d95f99c0a5e4b6d
FareedKandalaft <fareed@spectrecoder.com>
2021-10-20 22:31:09 -0400

    rename collections-overview component and styles

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selector';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview';
 
 const ShopPage = ({ collections }) => (
   <div className='shop-page'>
@@ -12,8 +9,4 @@ const ShopPage = ({ collections }) => (
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
