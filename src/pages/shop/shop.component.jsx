3d89f35479ebf8d08416e27150b7333ffe89b807
FareedKandalaft <fareed@spectrecoder.com>
2021-11-07 13:10:35 -0500

    implememt updateCollections as dispatch=>{} for firebase stored data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,8 @@
 import { extend } from 'lodash';
 import React from 'react';
 import { Route } from 'react-router';
+import { connect } from 'react-redux';
+import { updateCollections } from '../../redux/shop/shop.actions';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 import {
@@ -12,11 +14,14 @@ class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
+    const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
-    collectionRef.onSnapshot(async (snapshot) => {
-      const collection = convertCollectionsSnapshotToMap(snapshot);
-      console.log(collection);
-    });
+    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
+      async (snapshot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        updateCollections(collectionsMap);
+      }
+    );
   }
 
   render() {
@@ -33,4 +38,9 @@ class ShopPage extends React.Component {
   }
 }
 
-export default ShopPage;
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
